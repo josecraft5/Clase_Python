e982a1fbdd5e4adf225bac7a735a888e1a1683b1
estudiante <estudiante@usfq.edu.ec>
2026-02-24 08:16:38 -0500

    archivos actulizados

---
+++ main.py
@@ -0,0 +1,19 @@
+n1 = int(input("Cual es el numero cuantico principal: "))
+
+while n1 >=7 :
+    n1 = int(input("Ingresa bien el cuanto principal: "))
+if n1 == 1:
+    cuanto = 1
+elif n1 == 2:
+    cuanto = 2
+elif n1 == 3:
+    cuanto = 3
+elif n1 == 4:
+    cuanto = 4
+elif n1 == 5:
+    cuanto = 5
+elif n1 == 6:
+    cuanto = 6
+print(cuanto)
+        
+    
